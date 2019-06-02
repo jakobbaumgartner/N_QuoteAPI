f8bef25172e8d7af491dbbd5066892c2cdd76b94
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-06-02 16:59:20 +0200

    status codes

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.get('/tv', functontv);
 app.get('/rickandmorty',functionrickandmorty)
 
 app.use('/help', (req, res) => {
-	res.send("On this API you can get three different quotes in response. A random quote will  be returned based on request path variable. \n You can request: '/great', '/tv' or '/rickandmorty'.")
+	res.status(200).send("On this API you can get three different quotes in response. A random quote will  be returned based on request path variable. \n You can request: '/great', '/tv' or '/rickandmorty'.")
 	console.log("help")
 });
 
 app.use('/', (req, res) => {
-	res.send("This URL does not exist. Try '/help' ")
+	res.status(400).send("This URL does not exist. Try '/help' ")
 	console.log("not exist")
 });
 
--- controllers/servingfunctions.js
+++ controllers/servingfunctions.js
@@ -5,7 +5,7 @@ module.exports.functiongreat = (req, res) => {
 
 	var quote = greatquotes[Math.floor(Math.random()*80)]
 	console.log(quote)
-	res.send(JSON.stringify(quote))
+	res.status(200).send(JSON.stringify(quote))
 
 }
 
@@ -13,7 +13,7 @@ module.exports.functontv = (req, res) => {
 
 	var quote = tvquotes[Math.floor(Math.random()*137)]
 	console.log(quote)
-	res.send(JSON.stringify(quote))
+	res.status(200).send(JSON.stringify(quote))
 	
 }
 
@@ -21,7 +21,7 @@ module.exports.functionrickandmorty = (req, res) => {
 
 	var quote = rickandmortyquotes[Math.floor(Math.random()*126)]
 	console.log(quote)
-	res.send(JSON.stringify(quote))
+	res.status(200).send(JSON.stringify(quote))
 
 	
 }
