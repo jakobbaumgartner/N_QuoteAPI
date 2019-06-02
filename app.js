9fcfbda3c05057d7403c46e76b73b8c0fe8a3f35
jakobbaumgartner <jakob.baumgartner@hotmail.com>
2019-06-02 14:56:19 +0200

    quotes

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express()
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+	console.log(`Server started on port 3000`);
+});
